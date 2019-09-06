df227c82d84ba716ab2c2ec727d5ba28be601d78
alumno <alumno>
2019-09-06 10:28:34 -0300

    clase 5

---
+++ cadenas/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char palabra[10];
+    printf("ingrese una palabra ");
+    gets(palabra);
+
+    printf("%s",palabra);
+
+    return 0;
+}
